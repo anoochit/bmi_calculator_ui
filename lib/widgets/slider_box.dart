6239e2b3644e4f3fb1aed014b01d6a38848918a1
Anuchit Chalothorn <anoochit@gmail.com>
2022-07-19 20:43:51 +0700

    update repainted boundary

--- lib/widgets/slider_box.dart
+++ lib/widgets/slider_box.dart
@@ -39,68 +39,71 @@ class _SliderBoxState extends State<SliderBox> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: widget.width,
-      height: widget.height,
-      decoration: BoxDecoration(
-        color: Colors.white.withOpacity(0.05),
-        borderRadius: BorderRadius.circular(12.0),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // title
-          Text(
-            widget.title.toUpperCase(),
-            style: TextStyle(
-              color: Colors.white.withOpacity(0.4),
-              fontSize: 20.0,
-            ),
-          ),
-          const SizedBox(
-            height: 16.0,
-          ),
-
-          // value
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Text(
-                '${slideValue.floor()}',
-                style: TextStyle(
-                  color: widget.sliderActiveColor,
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold,
-                ),
+    return RepaintBoundary(
+      child: Container(
+        width: widget.width,
+        height: widget.height,
+        decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.05),
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // title
+            Text(
+              widget.title.toUpperCase(),
+              style: TextStyle(
+                color: Colors.white.withOpacity(0.4),
+                fontSize: 20.0,
               ),
-              Text(
-                widget.unit,
-                style: TextStyle(
-                  color: Colors.white.withOpacity(0.4),
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.w300,
-                ),
-              )
-            ],
-          ),
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
 
-          // slider
-          Slider(
-            activeColor: Colors.amber,
-            inactiveColor: Colors.amber.shade100,
-            min: widget.minValue,
-            max: widget.maxValue,
-            value: slideValue,
-            onChanged: (value) {
-              setState(() {
-                slideValue = value;
-                widget.onChanged(slideValue.floor());
-              });
-            },
-          ),
-        ],
+            // value
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  '${slideValue.floor()}',
+                  style: TextStyle(
+                    color: widget.sliderActiveColor,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  widget.unit,
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.4),
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w300,
+                  ),
+                )
+              ],
+            ),
+
+            // slider
+            Slider(
+              activeColor: Colors.amber,
+              inactiveColor: Colors.amber.shade100,
+              min: widget.minValue,
+              max: widget.maxValue,
+              value: slideValue,
+              onChanged: (value) {
+                setState(() {
+                  slideValue = value;
+                  widget.onChanged(slideValue.floor());
+                });
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
